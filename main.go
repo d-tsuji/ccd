9c1add74abc194ed6b056a116409278e140adfce
d-tsuji <dram.dt.shonan@gmail.com>
2020-05-07 21:21:10 +0900

    add: remove command

--- main.go
+++ main.go
@@ -190,6 +190,7 @@ var commandLs = &cli.Command{
 			panic(err)
 		}
 
+		// If a user tries to register an alias named "Quit", a bug will occur.
 		if text == "Quit" {
 			return nil
 		}
@@ -202,12 +203,77 @@ var commandLs = &cli.Command{
 	},
 }
 
-// TODO: Need implementation. Remove the config line and reconfigure the file.
 var commandRm = &cli.Command{
 	Name:    "remove",
 	Aliases: []string{"rm"},
 	Usage:   "remove the registered point",
 	Action: func(c *cli.Context) error {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return err
+		}
+		fpath := filepath.Join(home, ".config", "ccd", "config.tsv")
+		f, err := os.Open(fpath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		sc := bufio.NewScanner(f)
+
+		paths := []config{}
+		for sc.Scan() {
+			text := sc.Text()
+			s := strings.Split(text, "\t")
+			if len(s) != 2 {
+				fmt.Errorf("invalid config, please check the config.tsv")
+			}
+			alias, path := s[0], s[1]
+			fmt.Println(alias, path)
+			paths = append(paths, config{alias, path})
+		}
+
+		app := tview.NewApplication()
+		view := tview.NewList()
+		for i, p := range paths {
+			view.AddItem(fmt.Sprintf("%s -> %s\n", p.alias, p.path), p.alias, characters[i], nil)
+		}
+		view.AddItem("Quit", "Press to exit", 'q', func() { app.Stop() })
+		view.ShowSecondaryText(false)
+		var text string
+		var deleteAlias string
+		var index int
+		view.SetSelectedFunc(func(idx int, mainText string, secondaryText string, shortcut rune) {
+			index = idx
+			text = mainText
+			deleteAlias = secondaryText
+			app.Stop()
+		})
+		if err := app.SetRoot(view, true).EnableMouse(true).Run(); err != nil {
+			panic(err)
+		}
+
+		// If a user tries to register an alias named "Quit", a bug will occur.
+		if text == "Quit" {
+			return nil
+		}
+
+		// Make new file for deleting configuration.
+		f, err = os.Create(fpath)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		for _, p := range paths {
+			if p.alias == deleteAlias {
+				continue
+			}
+			if _, err = f.WriteString(fmt.Sprintf("%s\t%s\n", p.alias, p.path)); err != nil {
+				return err
+			}
+		}
+		Logf("info", "alias (%s) has been successfully deleted.", deleteAlias)
+
 		return nil
 	},
 }
